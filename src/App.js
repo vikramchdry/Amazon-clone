8d84bc4ad6f39177b83599f5d761c265a3a3d7b6
Vikram Singh <ec1029231058@gmail.com>
2020-12-04 20:58:28 +0100

    updated work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+import './script/App.css';
+import './script/Header.css';
 
-//import { BrowserRouter as Router, Route, Link , Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route , Switch} from "react-router-dom";
 
 import React from "react";
 
@@ -11,13 +12,38 @@ import Header from './Header';
 
 
 function App() {
+  
 
   return (
+
+    <Router>
       <div className = "app">
+        <Switch>
+        <Route path = "/product">
+            <h1>Hello Product!</h1>
+          </Route>
+        <Route path = "/checkout">
+            <h1>Hello Checkout !</h1>
+          </Route>
+          <Route path = "/payment">
+            <h1>Hello Payment!</h1>
+          </Route>
+          
+        <Route path = "/login">
+            <h1>Home Login Page !</h1>
+          </Route>
 
-        <h1>Hello World !</h1>
+          {/*  Default Router page */}
+        <Route path = "/">
+          <Header/>
+            <h1>Home Page!!!!!!!!!!!!!</h1>
+          </Route>
 
+        </Switch>
       </div>
+    </Router>
+
+      
   
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,38 @@
-import { ReactComponent } from "*.svg";
+
 import React from "react";
+import SearchIcon from  '@material-ui/icons/Search';
+import { Link } from "react-router-dom";
+
+
+
+{/* Logo on the left*/}
+
+{/* Search Box*/}
 
 
 function Header() {
 
     return (
+        
         <div className = "header">
+            <Link to = "/"> 
+            <img
+            className = "header__logo" alt = ""
 
-            <h1>Hello Header !</h1>
+            src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+
+            />
+            </Link>
+            
+        <div className="header__search">
+
+            <input className="header__searchInput" type="text"/>
+
+
+            <SearchIcon className="header__searchIcon" />
+
+        </div>
+
 
         </div>
     );

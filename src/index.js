c16c48088effe092052e4fd48b97372cc2ee0160
Vikram Singh <ec1029231058@gmail.com>
2020-12-06 03:37:02 +0100

    reducer to get all action and state info

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider';
+import {intialState} from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+
+export const initialState = {
+    basket:[],
+};
+// data in basket we call it state and everything we do like add to cart it call it action
+
+function reducer(state,action){
+    switch(action.type) {
+        case 'Add_TO_BASKET':
+    }
+} 
